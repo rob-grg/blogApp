b5258ffffaf5efba8aa04e29fe136cbceb7b2be6
rob-grg <robgurunghere@gmail.com>
2022-01-09 07:32:33 +0545

    init empty microservices

---
+++ discovery-service/discovery-service/src/test/java/com/rabin/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabin.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
